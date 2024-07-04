d1ea5c6f3ccbb10874fb56f975497dd2cd8a41da
Thiago Santos <thiagofrancisco627@gmail.com>
2024-07-04 01:14:40 -0300

    feat: deixando funcional as telas de exercicio e treino

---
+++ app/src/main/java/activities/Activity_ListaDeTreinos.java
@@ -0,0 +1,121 @@
+package activities;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.fitbuilder.R;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+import cruds.Friends;
+import cruds.Training;
+
+public class Activity_TreinosDaFicha extends AppCompatActivity {
+
+    ImageView btnVoltar;
+    Button btnAdicionar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_treinos_da_ficha);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.treinos_da_ficha_treinosDaFicha), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnVoltar = findViewById(R.id.treinos_da_ficha_btnvoltar);
+        btnAdicionar = findViewById(R.id.treinos_da_ficha_buttonAdicionarTreino);
+
+        Intent intent = getIntent();
+        String id = intent.getStringExtra("ID");
+
+        Training training = new Training(this);
+
+        LinearLayout buttonContainer = findViewById(R.id.button_container);
+
+        ArrayList<String> buttonNames = new ArrayList<>();
+        ArrayList<Integer> buttonIds = new ArrayList<>();
+
+        Cursor cursor = training.getAllTrainings();
+        if (cursor.moveToFirst()) {
+            do {
+                int indexNome = cursor.getColumnIndex("name");
+                int indexUserId = cursor.getColumnIndex("user_id");
+                int indexId = cursor.getColumnIndex("id");
+
+                String idUser = cursor.getString(indexUserId);
+                String nomeTraining = cursor.getString(indexNome);
+                String idTraining = cursor.getString(indexId);
+
+                if (Objects.equals(idUser, id)) {
+                    buttonNames.add(nomeTraining);
+                    buttonIds.add(Integer.parseInt(idTraining));
+                }
+
+            } while (cursor.moveToNext());
+        }
+
+        Integer index = 0;
+        for (String name : buttonNames) {
+            AppCompatButton button = new AppCompatButton(this);
+            button.setText(name);
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+            );
+            params.setMargins(0, 10, 0, 10);
+            button.setLayoutParams(params);
+
+            buttonContainer.addView(button);
+
+            Integer finalIndex = index;
+            button.setOnClickListener(v ->
+                    {
+                        Intent intent2 = new Intent(Activity_TreinosDaFicha.this, Activity_ListaDeExercicios.class);
+                        intent2.putExtra("ID_TRAINING", buttonIds.get(finalIndex));
+                        startActivity(intent2);
+
+                    }
+            );
+            index++;
+        }
+
+        btnAdicionar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Activity_TreinosDaFicha.this, Activity_CadastroTreino.class);
+                intent.putExtra("ID", id);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+}
+
